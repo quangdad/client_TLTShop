8d7123a11a3940d95a5f3284c069a28939225f44
Le Dat <bapxanh02@gmail.com>
2023-05-24 16:37:29 +0700

    push

---
+++ src/components/layout/slideCategory.js
@@ -0,0 +1,9 @@
+// import React, { Component } from "react";
+
+// class SlideCategoryy extends Component {
+//   render() {
+//     return <div>hihi</div>;
+//   }
+// }
+
+// export default SlideCategoryy;
